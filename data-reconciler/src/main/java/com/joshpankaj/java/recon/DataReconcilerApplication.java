37f29134de1d5d3f189be2cabee40ef6f9c75e53
joshpankaj <joshpankaj@gmail.com>
2021-05-09 14:37:03 +0530

    data-recon first commit

---
+++ data-reconciler/src/main/java/com/joshpankaj/java/recon/DataReconcilerApplication.java
@@ -0,0 +1,13 @@
+package com.joshpankaj.java.recon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataReconcilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataReconcilerApplication.class, args);
+	}
+
+}
